3148de07cd38a13fc9ffd5b0fdc2e6df755262cd
Laura Castro <lauracastrotech@gmail.com>
2025-05-22 17:48:36 -0400

    passes draws ten letters

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,27 @@
+
+const getRandomIdx = () => {
+  const NUM_OF_LETTERS_IN_LETTER_BANK = 98
+  return Math.floor(Math.random() * NUM_OF_LETTERS_IN_LETTER_BANK)
+}
 export const drawLetters = () => {
-  // Implement this method for wave 1
+  // 10 Letters in a hand
+  const NUM_OF_LETTERS_ALLOWED = 10
+  // letterBank
+  let letterBank = ['A','A','A','A','A','A','A','A','A', 'B','B', 'C', 'C', 'D', 'D', 'E', 'E','E','E','E','E','E','E','E','E','E','E', 'F', 'F', 'G', 'G', 'G', 'H', 'H', 'I', 'I','I','I','I','I','I','I','I','J', 'K', 'L','L','L','L', 'M', 'M', 'N','N','N','N','N','N', 'O', 'O','O','O','O','O','O','O', 'P', 'P', 'Q', 'R','R','R','R','R','R', 'S', 'S','S','S', 'T', 'T', 'T','T','T','T', 'U','U','U','U', 'V', 'V', 'W', 'W', 'X', 'Y', 'Y', 'Z'];
+  
+  // Player's hand
+  let hand = [];
+  // Run getLetterFromBank() 10 times
+  while(hand.length < NUM_OF_LETTERS_ALLOWED){
+    let letter = getRandomIdx();
+    if(letter !== null){
+        hand.push(letterBank[letter]);
+        // Set letter to null so that it can't be accessed again
+        letterBank[letter] = null;
+    }
+  }
+  // return hand
+  return hand
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
