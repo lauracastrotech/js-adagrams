c83b87dc165977e94de05c64d4db59b73cf8c35e
Laura Castro <lauracastrotech@gmail.com>
2025-05-23 18:27:11 -0400

    passes all test in wave 1

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,18 +1,55 @@
+const LETTER_POOL = {
+  A: 9,
+  B: 2,
+  C: 2,
+  D: 4,
+  E: 12,
+  F: 2,
+  G: 3,
+  H: 2,
+  I: 9,
+  J: 1,
+  K: 1,
+  L: 4,
+  M: 2,
+  N: 6,
+  O: 8,
+  P: 2,
+  Q: 1,
+  R: 6,
+  S: 4,
+  T: 6,
+  U: 4,
+  V: 2,
+  W: 2,
+  X: 1,
+  Y: 2,
+  Z: 1,
+};
 
 const getRandomIdx = () => {
-  const NUM_OF_LETTERS_IN_LETTER_BANK = 98
+  const NUM_OF_LETTERS_IN_LETTER_BANK = 96
   return Math.floor(Math.random() * NUM_OF_LETTERS_IN_LETTER_BANK)
+}
+
+const getLetterBank = (LETTER_POOL)=> {
+  let letterBank = []
+  for(let letter in LETTER_POOL){
+    const freq = LETTER_POOL[letter]
+	    letterBank.push(...letter.repeat(freq).split(''))
+  }
+  return letterBank
 }
 export const drawLetters = () => {
   const NUM_OF_LETTERS_ALLOWED = 10
-  let letterBank = ['A','A','A','A','A','A','A','A','A', 'B','B', 'C', 'C', 'D', 'D', 'E', 'E','E','E','E','E','E','E','E','E','E','E', 'F', 'F', 'G', 'G', 'G', 'H', 'H', 'I', 'I','I','I','I','I','I','I','I','J', 'K', 'L','L','L','L', 'M', 'M', 'N','N','N','N','N','N', 'O', 'O','O','O','O','O','O','O', 'P', 'P', 'Q', 'R','R','R','R','R','R', 'S', 'S','S','S', 'T', 'T', 'T','T','T','T', 'U','U','U','U', 'V', 'V', 'W', 'W', 'X', 'Y', 'Y', 'Z'];
+  let letterBank = getLetterBank(LETTER_POOL);
   let hand = [];
 
   while(hand.length < NUM_OF_LETTERS_ALLOWED){
-    let letter = getRandomIdx();
-    if(letter !== null){
-        hand.push(letterBank[letter]);
-        letterBank[letter] = null;
+    let randIdx = getRandomIdx();
+    if(letterBank[randIdx] !== null){
+        hand.push(letterBank[randIdx]);
+        letterBank[randIdx] = null;
     }
   }
   return hand
